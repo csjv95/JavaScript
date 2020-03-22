f91cea0e303869483b0608ec5a411481eab0bd75
csjv95 <csjv95@gmail.com>
2020-03-23 00:17:07 +0900

    docs : Algorithm ex1,ex2 ing

---
+++ ex2.js
@@ -0,0 +1,16 @@
+// #1
+// function linearSearch(array,target) {
+//   for(let i =0; i<array.length; i++) {
+//     if(array[i] === target){
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); 
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); 
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 5)); 
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); 
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); 
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 0)); 
+// console.log(linearSearch([1, 2, 3, 4, 5, 6], 7)); 
